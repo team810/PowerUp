05e80bd3b1488a4835647356e5e54b9005e73d4d
Team 810 <Team 810@10.198.29.110>
2018-02-07 17:01:16 -0500

    Added buttons for teleop

--- PowerUp/src/org/usfirst/frc/team810/robot/OI.java
+++ PowerUp/src/org/usfirst/frc/team810/robot/OI.java
@@ -7,7 +7,16 @@
 
 package org.usfirst.frc.team810.robot;
 
+import org.usfirst.frc.team810.robot.commands.Climb;
+import org.usfirst.frc.team810.robot.commands.ExtendHook;
+import org.usfirst.frc.team810.robot.commands.Intake;
+import org.usfirst.frc.team810.robot.commands.MoveArm;
+import org.usfirst.frc.team810.robot.commands.OneSideIntake;
+import org.usfirst.frc.team810.robot.commands.RetractHook;
+import org.usfirst.frc.team810.robot.commands.ToggleClaw;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -15,14 +24,42 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	public Joystick leftStick, rightStick;
+	public Joystick leftStick, rightStick, gamepad;
+	public JoystickButton intake, oneIntake, output, raiseArm, lowerArm, toggleClaw, extendHook, retractHook, climb;
 	
 	public OI() {
 		//Joysticks
 		leftStick = new Joystick(PortNumbers.LEFT_JOYSTICK);
 		rightStick = new Joystick(PortNumbers.RIGHT_JOYSTICK);
+		gamepad = new Joystick(PortNumbers.GAMEPAD);
 		
 		//Buttons
+		intake = new JoystickButton(rightStick, 1);
+		intake.whileHeld(new Intake(.4));
+		
+		oneIntake = new JoystickButton(leftStick, 1);
+		oneIntake.whileHeld(new OneSideIntake(.4));
+		
+		output = new JoystickButton(rightStick, 2);
+		output.whileHeld(new Intake(-.4));
+		
+		raiseArm = new JoystickButton(gamepad, 4);
+		raiseArm.whileHeld(new MoveArm(.4));
+		
+		lowerArm = new JoystickButton(gamepad, 2);
+		lowerArm.whileHeld(new MoveArm(-.4));
+		
+		toggleClaw = new JoystickButton(gamepad, 3);
+		toggleClaw.whenPressed(new ToggleClaw());
+		
+		extendHook = new JoystickButton(gamepad, 6);
+		extendHook.whenPressed(new ExtendHook(2));
+		
+		retractHook = new JoystickButton(gamepad, 8);
+		retractHook.whenPressed(new RetractHook(2));
+		
+		climb = new JoystickButton(gamepad, 5);
+		climb.whileHeld(new Climb(1));
 		
 		//SmartDashboard values
 		SmartDashboard.putNumber("kP", 0);
--- PowerUp/src/org/usfirst/frc/team810/robot/PortNumbers.java
+++ PowerUp/src/org/usfirst/frc/team810/robot/PortNumbers.java
@@ -5,6 +5,7 @@ public class PortNumbers {
 	//USB
 	public static final int LEFT_JOYSTICK = 1;
 	public static final int RIGHT_JOYSTICK = 0;
+	public static final int GAMEPAD = 2;
 	
 	//PWM
 	public static final int DRIVE_FRONT_LEFT = 0;
@@ -16,10 +17,12 @@ public class PortNumbers {
 	public static final int ARM_MOTOR = 6;
 	public static final int HOOK_MOTOR = 7;
 	public static final int CLIMB_MOTOR = 8;
-	public static final int POT = 9;
 	
 	//PCM
 	public static final int INTAKE_PISTON = 0;
 	public static final int CLAW = 1;
 	public static final int SPRING_PISTON = 2;
+	
+	//Analog
+	public static final int POT = 0;
 }
